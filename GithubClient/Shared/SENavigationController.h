6138623a4223923b6f27af6efc84edf882afc0e9
Evseev Sergey <evseev.srg@gmail.com>
2015-04-04 23:01:17 +0300

    NavigationController added

---
+++ GithubClient/Shared/SENavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SENavigationControllerViewController.h
+//  GithubClient
+//
+//  Created by Evseev Sergey on 04/04/15.
+//  Copyright (c) 2015 Sergey Evseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SENavigationController : UINavigationController
+
+@end
